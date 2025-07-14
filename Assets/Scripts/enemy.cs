24771bb166f5224408b2b5e81a0a138c378f4cb4
aziz1905 <azizcube1905@gmail.com>
2025-07-14 10:14:08 +0700

    Enemy
    +Enemy

---
+++ Assets/Scripts/enemy.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemy : MonoBehaviour
+{
+    private Transform playerTransform;
+    private NavMeshAgent nav;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nav.destination = playerTransform.position;
+    }
+}
